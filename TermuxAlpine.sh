50b3612c8a6fdfce0eadbee57eee3406a1766446
Smith Dhumbumroong <zodmaner@gmail.com>
2024-11-21 22:24:41 +0700

    Integrate finaltouchup script into main script

--- TermuxAlpine.sh
+++ TermuxAlpine.sh
@@ -4,6 +4,8 @@
 # Email : lkpandey950@gmail.com
 ################################################################################
 
+set -eux
+
 # colors
 
 red='\033[1;31m'
@@ -176,11 +178,10 @@ remove_installation_files() {
 
 finalwork() {
     if [ "${MOTD}" = "ON" ]; then
-        bash ${DESTINATION}/finaltouchup.sh --add-motd
+        finaltouchup --add-motd
     else
-        bash ${DESTINATION}/finaltouchup.sh
+        finaltouchup
     fi
-    rm ${DESTINATION}/finaltouchup.sh
 }
 
 # Utility function for cleanup
@@ -254,3 +255,67 @@ printline
 printf "$red \n NOTE : $yellow use ${red}--uninstall${yellow} option for uninstall\n"
 printline
 printf "$reset"
+
+ALPINE_PROOT=${PREFIX}/.local/share/proot/alpine
+
+addprofile()
+{
+	cat > ${ALPINE_PROOT}/etc/profile <<- EOM
+	export CHARSET=UTF-8
+	export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+	export PAGER=less
+	export PS1='[\u@\h \W]\\$ '
+	umask 022
+	for script in /etc/profile.d/*.sh ; do
+	if [ -r \$script ] ; then
+	. \$script
+	fi
+	done
+	EOM
+}
+
+addmotd() {
+	cat > ${ALPINE_PROOT}/etc/profile.d/motd.sh  <<- EOM
+	printf "\n\033[1;34mWelcome to Alpine Linux in Termux!  Enjoy!\033[0m\033[1;34m
+	Chat:    \033[0m\033[mhttps://gitter.im/termux/termux/\033[0m\033[1;34m
+		Help:    \033[0m\033[34minfo <query> \033[0m\033[mand \033[0m\033[34mman <query> \033[0m\033[1;34m
+			Portal:  \033[0m\033[mhttps://wiki.termux.com/wiki/Community\033[0m\033[1;34m
+
+		Install a package: \033[0m\033[34mapk add <package>\033[0m\033[1;34m
+			More  information: \033[0m\033[34mapk --help\033[0m\033[1;34m
+				Search   packages: \033[0m\033[34mapk search <query>\033[0m\033[1;34m
+					Upgrade  packages: \033[0m\033[34mapk upgrade \n\033[0m \n"
+						EOM
+}
+
+updrepos() {
+	cp ${ALPINE_PROOT}/etc/apk/repositories ${ALPINE_PROOT}/etc/apk/repositories.bak
+	cat > ${ALPINE_PROOT}/etc/apk/repositories <<- EOM
+	http://dl-cdn.alpinelinux.org/alpine/latest-stable/main/
+	http://dl-cdn.alpinelinux.org/alpine/latest-stable/community/
+	http://dl-cdn.alpinelinux.org/alpine/edge/testing/
+	EOM
+}
+
+# thnx to @j16180339887 for DNS picker 
+addresolvconf ()
+{
+	printf "nameserver 8.8.8.8\nnameserver 8.8.4.4" > ${ALPINE_PROOT}/etc/resolv.conf
+}
+
+finaltouchup()
+{    
+    android=$(getprop ro.build.version.release)
+
+    addprofile
+
+    if [ "${1}" = "--add-motd" ]; then
+	    addmotd
+    fi
+
+    if [ ${android%%.*} -ge 8 ]; then
+	    addresolvconf
+    fi
+
+    updrepos
+}
